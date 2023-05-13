b624fadc1fd5ce05a979f3fffe39392478899333
Bilal Sirazitdinov <sirazitdinov.b@samalsoft.com>
2023-05-13 22:58:27 +0600

    testing configs

--- rollup.config.js
+++ rollup.config.js
@@ -4,36 +4,41 @@ import typescript from "@rollup/plugin-typescript";
 import terser from '@rollup/plugin-terser';
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import postcssModules from 'postcss-modules';
 
-// const packageJson = require("./package.json");
-import * as packageJson from "./package.json"  assert { type: "json" };
+import * as packageJson from "./package.json" assert {type: "json"};
 
 export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
-    ],
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts.default()],
-  },
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: packageJson.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({tsconfig: "./tsconfig.json"}),
+            // terser(),
+            postcss({
+              plugins: [postcssModules()]
+            })
+        ],
+    },
+    {
+        input: "dist/esm/types/index.d.ts",
+        output: [{file: "dist/index.d.ts", format: "esm"}],
+        plugins: [dts.default()],
+        external: [/\.less$/],
+    },
 ];
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+// import './Button.less';
+import style from './Button.module.less';
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className={style.btn}>{props.label}</button>;
 };
 
 export default Button;
